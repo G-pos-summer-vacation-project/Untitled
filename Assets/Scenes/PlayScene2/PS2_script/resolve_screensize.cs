7fe6d58e7c86cc2390b4d597a47d4fe21579ce87
Hyeonjung Kim <omyjjang11@gmail.com>
2022-07-28 23:04:23 +0900

    fixed merging error

--- Assets/Scenes/PlayScene2/PS2_script/resolve_screensize.cs
+++ Assets/Scenes/PlayScene2/PS2_script/resolve_screensize.cs
@@ -10,17 +10,18 @@ public class resolve_screensize : MonoBehaviour
         GameObject square_left = square.transform.GetChild(1).gameObject;
         GameObject square_right = square.transform.GetChild(3).gameObject;
 
+        var nowPos = square.transform.localPosition;
         var camera = GetComponent<Camera>();
         var size = camera.orthographicSize;
         Debug.Log(size);
         float scaleHeight = ((float)Screen.width / Screen.height) / ((float)9 / 16);
 
         var position = square_left.transform.localPosition;
-        position.x *= scaleHeight;
+        position.x = (position.x+nowPos.x) * scaleHeight - nowPos.x;
         square_left.transform.localPosition = position;
 
         position = square_right.transform.localPosition;
-        position.x *= scaleHeight;
+        position.x = (position.x + nowPos.x) * scaleHeight - nowPos.x;
         square_right.transform.localPosition = position;
     }
 }
